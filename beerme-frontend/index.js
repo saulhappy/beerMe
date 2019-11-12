b776379bc776982f802b3303799d9c6c18732deb
Joellehelm <joellenhelm@gmail.com>
2019-11-12 10:40:32 -0600

    added show page for single beer when it's clicked on.

--- beerme-frontend/index.js
+++ beerme-frontend/index.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function(){
     // uncomment the getBeers function and addBeer function to populate the database. 
     // getBeers();
 
+    let browseBeersContainer = document.getElementById("browse-beers-container")
+    let showBeerContainer = document.getElementById("show-beer-container")
+
     fetchBeers()
 
 
-})
+
 
 // function getBeers(){
 //     fetch("https://api.punkapi.com/v2/beers?page=4&per_page=60")
@@ -38,7 +41,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 function fetchBeers(){
+    showBeerContainer.style.visibility = "hidden";
+    browseBeersContainer.style.visibility = "visible";
     let beerList = document.getElementById("beer-list")
+
     
     fetch("http://localhost:3000/beers")
     .then(r => r.json())
@@ -47,16 +53,79 @@ function fetchBeers(){
             let card = document.createElement("card")
             let li = document.createElement("li")
             let image = document.createElement("img")
+    
 
             li.innerText = beer.name
             image.src = beer.image_url
+            image.style.height = "200px"
+            image.style.width = "65px"
+            image.dataset.id = beer.id
 
             card.append(li, image)
             beerList.append(card)
+            card.addEventListener("click", showBeer)
 
         })
     })
+
 }
+
+
+
+
+function showBeer(event){
+    let selectedBeer = event.target.dataset.id
+    browseBeersContainer.style.visibility = "hidden";
+    showBeerContainer.style.visibility = "visible";
+
+
+    fetch(`http://localhost:3000/beers/${selectedBeer}`)
+    .then(r => r.json)
+    .then(beer => {
+        let showBeerDiv = document.getElementById("single-beer")
+        let beerCard = document.createElement("card")
+        let NameLi = document.createElement("li")
+        NameLi.innerText = beer.name
+        let taglineLi = document.createElement("li")
+        taglineLi.innerText = beer.tagline
+        let abvLi = document.createElement("li")
+        abvLi.innerText = beer.abv
+        // let type = document.createElement("li")
+
+        let ibuLi = document.createElement("li")
+        ibuLi.innerText = beer.ibu
+        let description = document.createElement("li")
+        description.innerText = beer.description
+        let beerImage = document.createElement("img")
+        beerImage.src = beer.image_url
+        let ul = document.createElement("ul")
+
+
+        ul.append(abvLi, ibuLi)
+
+        beerCard.append(NameLi, taglineLi, beerImage, description, ul)
+
+
+        showBeerDiv.append(beerCard)
+
+
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
 
 
 
