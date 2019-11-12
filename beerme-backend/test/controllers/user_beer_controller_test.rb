26bf58dd88324f3ac9abc25a5f98ecb29bbb3cca
Alec Reid <alecreid0093@gmail.com>
2019-11-12 12:59:33 -0600

    controllers

--- beerme-backend/test/controllers/user_beer_controller_test.rb
+++ beerme-backend/test/controllers/user_beer_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserbeerControllerTest < ActionDispatch::IntegrationTest
+class UserBeersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
